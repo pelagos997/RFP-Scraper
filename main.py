4d31ef2694d4e9a6cef97b4d9014442ef65797ab
pelagos997 <rjones@ftandc.com>
2025-01-29 12:16:06 -0600

    Added Ollama funcationality to parse data

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from scrape import extract_download_links
-from scrape import scrape_website
-from scrape import (
-    extract_body_content, 
-    clean_body_content, 
-    split_dom_content
-)
+from scrape import extract_download_links, scrape_website
+from scrape import extract_body_content, clean_body_content, split_dom_content
+
+from parse import parse_with_ollama
+
+# Initialize session state
+if "dom_content" not in st.session_state:
+    st.session_state.dom_content = ""
 
 st.title('RFP Scraper')
 url = st.text_input("Enter a Website Url: ")
@@ -16,19 +17,28 @@ if st.button("Scrape"):
     body_content = extract_body_content(result)
     cleaned_content = clean_body_content(body_content)
     
-    
     # Extract and organize download links
-    # Pass the original URL for resolving relative paths
     download_links = extract_download_links(result, base_url=url)  
     
     st.session_state.dom_content = cleaned_content
     st.session_state.download_links = download_links
 
     with st.expander("View Content"):
-        st.text_area("DOM Content", cleaned_content, height = 300)
+        st.text_area("DOM Content", cleaned_content, height=300)
     st.subheader("Downloadable Files")
     for category, links in download_links.items():
         if links:  # Only show categories with links
             with st.expander(f"{category} ({len(links)})"):
                 for link in links:
                     st.markdown(f"[{link}]({link})")
+
+if st.session_state.dom_content:
+    parse_description = st.text_area("What do you want to parse?")
+    
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing Content")
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
+    
